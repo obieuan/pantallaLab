9339e8dfb376cf980e6d960aa79134920f30c045
obieuan <obieuan@gmail.com>
2024-04-30 14:32:07 -0600

    Diseño de alertdialog al tocar un espacio

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main(page: Page):
         bgcolor = '#1EF50A',
         on_change=print("hola")
         #overlay_color ="#1EF50A2",
-    )    
+    )
 
     def close_dlg(e):
         if dlg_modal is not None:
@@ -46,16 +46,25 @@ def main(page: Page):
         nonlocal dlg_modal
         dlg_modal = ft.AlertDialog(
             modal=True,
-            title=ft.Text(f"Desbloquear {button_id}"),
-            content=ft.Text("¿Confirmar acción?"),
+            title=ft.Text(f"Desbloquear mesa {button_id}",size=30,text_align=ft.TextAlign.CENTER),
+            icon = ft.Icon(name=ft.icons.TABLE_RESTAURANT, color=ft.colors.GREEN_400, size=40),
+            content=ft.Text("¿Confirmar acción?",size=25,text_align=ft.TextAlign.CENTER),
             actions=[
-                ft.TextButton("Sí", on_click=close_dlg),
-                ft.TextButton("No", on_click=close_dlg),
+                ft.IconButton(icon=ft.icons.CHECK_CIRCLE,
+                    icon_color=ft.colors.GREEN_400,
+                    icon_size=50,
+                    tooltip="Aceptar",
+                    on_click=close_dlg),
+                ft.IconButton(icon=ft.icons.CANCEL,
+                    icon_color=ft.colors.RED_400,
+                    icon_size=50,
+                    tooltip="Cancelar",
+                    on_click=close_dlg)
             ],
-            actions_alignment=ft.MainAxisAlignment.END,
+            actions_alignment=ft.MainAxisAlignment.CENTER,
             on_dismiss=lambda e: print("Modal dialog dismissed!"),
         )
-        return dlg_modal    
+        return dlg_modal
 
     def handle_button_click(e, button_id):
         print(f"Button {button_id} was pressed")
@@ -64,7 +73,7 @@ def main(page: Page):
         dlg_modal.open = True
         page.update()
 
-    def EspacioButton(texto, subtexto, on_click):
+    def EspacioButton(button_id, texto, subtexto, on_click):
         return ft.Container(
             content=ft.Container(
                 content=ft.Column(
@@ -75,53 +84,56 @@ def main(page: Page):
                     alignment=ft.MainAxisAlignment.CENTER,                    
                 ),
                 alignment=ft.alignment.center,
-                bgcolor='#0A3C82',                
+                bgcolor='#0A3C82',
                 height = 115,
                 width = 135,
                 border_radius=10,
                 ink=True,
-                on_click=on_click,
+                on_click=lambda e: on_click(e, button_id),
             ),
     )
 
-    def createRowOfButtons(buttons):
-        return Row(controls=[
-            EspacioButton(text, status, lambda e, text=text: handle_button_click(e, text))
-            for text, status in buttons
-        ], alignment=ft.MainAxisAlignment.END)
+    def createRowOfButtons(buttons, start_id=1):
+        return ft.Row(
+            controls=[
+                EspacioButton(button_id, text, status, lambda e, button_id=button_id: handle_button_click(e, button_id))
+                for button_id, (text, status) in enumerate(buttons, start=start_id)
+            ],
+            alignment=ft.MainAxisAlignment.END
+        )   
 
     page.add(
         ft.SafeArea(
             ft.Row([
                 ft.Container(
-                    content=Column(
+                    content=ft.Column(
                         controls=[
-                            createRowOfButtons([("Mesa 1", "Disponible"), ("Mesa 2", "Disponible"), ("Mesa 3", "Disponible")]),
-                            createRowOfButtons([("Mesa 4", "Disponible"), ("Mesa 5", "Disponible"), ("Mesa 6", "Disponible"), ("Mesa 7", "Disponible")]),
-                            createRowOfButtons([("Mesa 8", "Disponible"), ("Mesa 9", "Disponible"), ("Mesa 10", "Disponible"), ("Mesa 11", "Disponible"), ("Mesa 12", "Disponible")]),
+                            createRowOfButtons([("Mesa 1", "Disponible"), ("Mesa 2", "Disponible"), ("Mesa 3", "Disponible")], start_id=1),
+                            createRowOfButtons([("Mesa 4", "Disponible"), ("Mesa 5", "Disponible"), ("Mesa 6", "Disponible"), ("Mesa 7", "Disponible")], start_id=4),
+                            createRowOfButtons([("Mesa 8", "Disponible"), ("Mesa 9", "Disponible"), ("Mesa 10", "Disponible"), ("Mesa 11", "Disponible"), ("Mesa 12", "Disponible")], start_id=8),
                         ],
                         alignment=ft.MainAxisAlignment.END,
                     ),
-                    bgcolor=colors.SURFACE_VARIANT,
+                    bgcolor=ft.colors.SURFACE_VARIANT,
                     border_radius=20,
                     padding=20,
                     height=400,
                     width=770,
                 ),
                 ft.Container(
-                    content=Column(
+                    content=ft.Column(
                         controls=[
-                            Column(
+                            ft.Column(
                                 controls=[
-                                    EspacioButton("Soldadura 1", "Disponible", lambda e, text="Soldadura 1": handle_button_click(e, text)),
-                                    EspacioButton("Soldadura 2", "Disponible", lambda e, text="Soldadura 2": handle_button_click(e, text)),
+                                    EspacioButton(13, "Soldadura 1", "Disponible", handle_button_click),
+                                    EspacioButton(14, "Soldadura 2", "Disponible", handle_button_click),
                                 ],
                                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                             )
                         ],
                         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     ),
-                    bgcolor=colors.SURFACE_VARIANT,
+                    bgcolor=ft.colors.SURFACE_VARIANT,
                     border_radius=20,
                     padding=20,
                     height=400,
